f1a88386c8b424097bdf6d900e808d1a59e0f341
QiZhao <956361916@qq.com>
2018-10-30 11:43:22 +0800

    feat: 基本实现通知的WebService

--- Notice.asmx.cs
+++ Notice.asmx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,12 +17,19 @@ namespace SnnuWebService
     // [System.Web.Script.Services.ScriptService]
     public class Notice : System.Web.Services.WebService
     {
+        private DAL.Message dal = new DAL.Message();
 
         [WebMethod(Description =
             "所支持查询的部门")]
         public string [] getSupportDep()
         {
             List<string> ret = new List<string>();
+            MySqlDataReader reader = dal.AllDep();
+            while(reader.Read())
+            {
+                ret.Add(reader.GetValue(0).ToString());
+            }
+            reader.Close();
             return ret.ToArray();
         }
         [WebMethod(Description =
@@ -29,20 +37,65 @@ namespace SnnuWebService
         public Model.Message[]getNoticeByDepartment(string dep)
         {
             List<Model.Message> ret = new List<Model.Message>();
+            MySqlDataReader reader = dal.QueryByDepartment(dep,"通知");
+            while (reader.Read())
+            {
+                Model.Message temp = new Model.Message();
+                temp.Department = reader["Department"].ToString();
+                temp.Title = reader["Title"].ToString();
+                temp.Link = reader["Link"].ToString();
+                temp.Type = "通知";
+                temp.Date = DateTime.Parse(reader["Date"].ToString());
+                ret.Add(temp);
+            }
             return ret.ToArray();
         }
         [WebMethod(Description =
-            "查询以某个日期为准前后两周的通知")]
+            "查询以某个日期为准前后一周的通知")]
         public Model.Message[]getNoticeByDate(string date)
         {
             List<Model.Message> ret = new List<Model.Message>();
+            DateTime d;
+            bool flag=DateTime.TryParse(date, out d);
+            if (flag == false)
+                return null;
+            DateTime start = d.AddDays(-7);
+            DateTime end = d.AddDays(7);
+            MySqlDataReader reader = dal.QueryByDate(start, end, "通知");
+            while (reader.Read())
+            {
+                Model.Message temp = new Model.Message();
+                temp.Department = reader["Department"].ToString();
+                temp.Title = reader["Title"].ToString();
+                temp.Link = reader["Link"].ToString();
+                temp.Type = "通知";
+                temp.Date = DateTime.Parse(reader["Date"].ToString());
+                ret.Add(temp);
+            }
             return ret.ToArray();
         }
         [WebMethod(Description =
-            "查询某个部门的某个日期前后两周的通知")]
+            "查询某个部门的某个日期前后一周的通知")]
         public Model.Message[]getNoticeByDepartmentAndDate(string dep,string date)
         {
             List<Model.Message> ret = new List<Model.Message>();
+            DateTime d;
+            bool flag = DateTime.TryParse(date, out d);
+            if (flag == false)
+                return null;
+            DateTime start = d.AddDays(-7);
+            DateTime end = d.AddDays(7);
+            MySqlDataReader reader = dal.QueryByDateAndDep(start, end, dep,"通知");
+            while (reader.Read())
+            {
+                Model.Message temp = new Model.Message();
+                temp.Department = reader["Department"].ToString();
+                temp.Title = reader["Title"].ToString();
+                temp.Link = reader["Link"].ToString();
+                temp.Type = "通知";
+                temp.Date = DateTime.Parse(reader["Date"].ToString());
+                ret.Add(temp);
+            }
             return ret.ToArray();
         }
         [WebMethod(Description =
@@ -50,6 +103,17 @@ namespace SnnuWebService
         public Model.Message[]getNoticeByLikeTitle(string keyword)
         {
             List<Model.Message> ret = new List<Model.Message>();
+            MySqlDataReader reader = dal.QueryByLikeTitle(keyword, "通知");
+            while(reader.Read())
+            {
+                Model.Message temp = new Model.Message();
+                temp.Department = reader["Department"].ToString();
+                temp.Title = reader["Title"].ToString();
+                temp.Link = reader["Link"].ToString();
+                temp.Type = "通知";
+                temp.Date = DateTime.Parse(reader["Date"].ToString());
+                ret.Add(temp);
+            }
             return ret.ToArray();
         }
     }
