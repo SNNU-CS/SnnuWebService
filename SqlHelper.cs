57cc5f6e4c39da02022359665aec489ed545c6ba
QiZhao <zhaoqi99@outlook.com>
2018-10-31 11:38:02 +0800

    style: 删除不必要的using

--- SqlHelper.cs
+++ SqlHelper.cs
@@ -1,12 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.OleDb;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnnuWebService
 {
