a3788ec624080c35bb8ad20483b668e2309cf2e4
QiZhao <zhaoqi99@outlook.com>
2018-11-02 15:38:53 +0800

    添加CardItem实体类,相关web服务

---
+++ CampusCard.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SnnuWebService
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod(Description =
+            "查询校园卡余额")]
+        public double getBalance(string id)
+        {
+            double ret=0;
+            return ret;
+        }
+        [WebMethod(Description =
+            "查询消费明细")]
+        public Model.CardItem[] getConsumptionDdetails(string id)
+        {
+            List<Model.CardItem> ret = new List<Model.CardItem>();
+            return ret.ToArray();
+        }
+        [WebMethod(Description =
+            "查询最后一次消费时间")]
+        public DateTime getLastConsumptionTime(string id)
+        {
+            DateTime ret = DateTime.MinValue;
+            return ret;
+        }
+    }
+}
---
+++ Model/CardItem.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnnuWebService.Model
+{
+    public class CardItem
+    {
+        private string id;
+        private DateTime date = DateTime.MinValue;
+        private int frequency;
+        private double origiAmount;
+        private double transAmount;
+        private double balance;
+        private string location = string.Empty;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+
+        public int Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+
+            set
+            {
+                frequency = value;
+            }
+        }
+
+        public double OrigiAmount
+        {
+            get
+            {
+                return origiAmount;
+            }
+
+            set
+            {
+                origiAmount = value;
+            }
+        }
+
+        public double TransAmount
+        {
+            get
+            {
+                return transAmount;
+            }
+
+            set
+            {
+                transAmount = value;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+
+            set
+            {
+                location = value;
+            }
+        }
+    }
+}
