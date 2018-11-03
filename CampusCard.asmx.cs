4848c03d1ffbd4013f0a291db2195d929130227c
QiZhao <zhaoqi99@outlook.com>
2018-11-03 20:13:30 +0800

    feat: 查询校园卡消费信息

--- CampusCard.asmx.cs
+++ CampusCard.asmx.cs
@@ -14,21 +14,44 @@ namespace SnnuWebService
     [System.ComponentModel.ToolboxItem(false)]
     // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
     // [System.Web.Script.Services.ScriptService]
-    public class WebService1 : System.Web.Services.WebService
+    public class CampusCard : System.Web.Services.WebService
     {
-
+        SpiderCard spider = new SpiderCard();
         [WebMethod(Description =
             "查询校园卡余额")]
         public double getBalance(string id)
         {
-            double ret=0;
+            double ret = 0;
+            spider.Search(id);
+            if (spider.BeingUsed())
+                ret = Convert.ToDouble(spider.getBalance());
+            else
+                ret = -1;
             return ret;
         }
         [WebMethod(Description =
-            "查询消费明细")]
+            "查询最近一周的消费明细")]
         public Model.CardItem[] getConsumptionDdetails(string id)
         {
             List<Model.CardItem> ret = new List<Model.CardItem>();
+            List<Dictionary<string, string>> ListDic = new List<Dictionary<string, string>>();
+            spider.Search(id);
+            if (spider.BeingUsed())
+            {
+                ListDic = spider.ConsumptionDetails();
+            }
+            foreach (Dictionary<string, string> dic in ListDic)
+            {
+                Model.CardItem t = new Model.CardItem();
+                t.Balance = Convert.ToDouble(dic["卡余额"].ToString());
+                t.Date = DateTime.Parse(dic["时间"].ToString());
+                t.Id = dic["卡号"].ToString();
+                t.Frequency = Convert.ToInt32(dic["次数"].ToString());
+                t.OrigiAmount = Convert.ToDouble(dic["原金额"].ToString());
+                t.TransAmount = Convert.ToDouble(dic["交易额"].ToString());
+                t.Location = dic["记录信息"].ToString();
+                ret.Add(t);
+            }
             return ret.ToArray();
         }
         [WebMethod(Description =
@@ -36,6 +59,9 @@ namespace SnnuWebService
         public DateTime getLastConsumptionTime(string id)
         {
             DateTime ret = DateTime.MinValue;
+            spider.Search(id);
+            if (spider.BeingUsed())
+                ret = DateTime.Parse(spider.getLastConsumptionTime());
             return ret;
         }
     }
---
+++ Spider/SpiderCard.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace SnnuWebService
+{
+    public class SpiderCard : Spider
+    {
+        private string[] data = { "卡号", "时间", "次数", "原金额", "交易额", "卡余额", "记录信息" };
+        string pattern = @"<td>([\d.\s:\w-]+?)</td>";
+        private string html = string.Empty;
+        public SpiderCard() : base()
+        {
+            this.Url = "http://edutech.snnu.edu.cn/ecard/ccc.asp";
+            Method = "POST";
+            Create();
+            request.ContentType = "application/x-www-form-urlencoded";
+        }
+        public string Search(string Id)
+        {
+            CookieContainer cookie = new CookieContainer();
+            request.Headers.Add("Cookie", cookie.ToString());
+            request.AllowAutoRedirect = true;
+            string PostStr = string.Format("usernum={0}&search=查询&wx=", Id);
+            byte[] Array = Encoding.UTF8.GetBytes(PostStr);
+            request.ContentLength = Encoding.UTF8.GetByteCount(PostStr);
+            System.IO.Stream MyStream = request.GetRequestStream();
+            MyStream.Write(Array, 0, Array.Length);
+            string reader = string.Empty;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                response.Cookies = cookie.GetCookies(response.ResponseUri);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encode));
+                    reader = sr.ReadToEnd();
+                    sr.Close();
+                    response.Close();//关闭response响应流
+                }
+                else
+                    throw new Exception();
+            }
+            catch
+            {
+                //Todo:Hanle Exception
+            }
+            html = reader;
+            return reader;
+        }
+
+        public bool BeingUsed()
+        {
+            if (Regex.IsMatch(html,pattern))
+                return true;
+            else
+                return false;
+        }
+        public List<Dictionary<string, string>> ConsumptionDetails()
+        {
+            List<Dictionary<string, string>> DATA = new List<Dictionary<string, string>>();
+            Dictionary<string, string> x = new Dictionary<string, string>();
+            int num = 0;
+            foreach (Match match in Regex.Matches(html, pattern))
+            {
+                if (num != 0 && num % 7 == 0)
+                {
+                    DATA.Add(x);
+                    x = new Dictionary<string, string>();
+                }
+                x.Add(data[num % 7], match.Groups[1].Value);
+                num++;
+            }
+            return DATA;
+        }
+        public string getLastConsumptionTime()
+        {
+            int num = 1;
+            string ret = string.Empty;
+            foreach (Match match in Regex.Matches(html, pattern))
+            {
+                if (num == 2)
+                {
+                    ret = match.Groups[1].Value.ToString();
+                    break;
+                }
+                num++;
+            }
+            return ret;
+        }
+       public  string getBalance()
+        {
+            int num = 1;
+            string ret = string.Empty;
+            foreach (Match match in Regex.Matches(html, pattern))
+            {
+                if (num == 6)
+                {
+                    ret = match.Groups[1].Value.ToString();
+                    break;
+                }
+                num++;
+            }
+            return ret;
+        }
+
+    }
+}
