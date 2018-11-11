0dafc8a5270acd8b173cce3880957c10f4d7c26b
QiZhao <zhaoqi99@outlook.com>
2018-11-12 00:31:11 +0800

    修改webservice的namespace

--- Libiary.asmx.cs
+++ Libiary.asmx.cs
@@ -9,7 +9,7 @@ namespace SnnuWebService
     /// <summary>
     /// Libiary 的摘要说明
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://webxml.zhaoqi.vip/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
