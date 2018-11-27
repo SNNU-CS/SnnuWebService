1999b2d7ba613b937c4b8c1becdab3139aff2e70
Qi Zhao <zhaoqi99@outlook.com>
2018-11-27 15:09:18 +0800

    Notice Demo

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WebServiceDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxDate = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBoxDate
+            // 
+            this.TextBoxDate.Location = new System.Drawing.Point(250, 39);
+            this.TextBoxDate.Name = "TextBoxDate";
+            this.TextBoxDate.Size = new System.Drawing.Size(133, 25);
+            this.TextBoxDate.TabIndex = 0;
+            this.TextBoxDate.Text = "2018-08-25";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 96);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(379, 382);
+            this.textBox2.TabIndex = 1;
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Location = new System.Drawing.Point(466, 32);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(96, 35);
+            this.BtnOk.TabIndex = 2;
+            this.BtnOk.Text = "确认";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(455, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(379, 382);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(115, 504);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "序列化后数据";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(600, 504);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "请求报文";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 544);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TextBoxDate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxDate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebServiceDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.NoticeSoapClient s = new ServiceReference1.NoticeSoapClient();
+            try
+            {
+                ServiceReference1.Message[] ret = s.getNoticeByDate(TextBoxDate.Text.ToString());
+                foreach (var t in ret)
+                {
+                    textBox2.Text += ToString(t);
+                }
+                textBox1.Text = getXmlText();
+            }
+            catch
+            {
+                MessageBox.Show("请检查网络");
+            }
+        }
+        private string ToString(ServiceReference1.Message m)
+        {
+            return string.Format("标题:{0} 时间:{1} 链接:{2}", m.Title, m.Date.ToString("yyyy-MM-dd"), m.Link);
+
+        }
+        private string getXmlText()
+        {
+            string service = "http://webxml.zhaoqi.vip/Notice.asmx/getNoticeByDate";
+            string contenttype = "application/x-www-form-urlencoded";//更网站该方法支持的类型要一致
+                                                                     //根据接口，写参数
+            string para = "date="+TextBoxDate.Text.ToString();
+            //+ TextBoxDate.ToString();
+            //发送请求
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(service);
+            myRequest.Method = "POST";
+            myRequest.ContentType = contenttype;
+            myRequest.ContentLength = para.Length;
+            Stream newStream = myRequest.GetRequestStream();
+            // Send the data.
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postdata = encoding.GetBytes(para);
+            newStream.Write(postdata, 0, para.Length);
+            newStream.Close();
+            // Get response
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+            string content = reader.ReadToEnd();//得到结果
+            return content;
+        }
+    }
+}
